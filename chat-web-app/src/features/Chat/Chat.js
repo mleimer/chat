cf52417aaedda5f270c37776e27d4e8cca913d6e
Martin Leimer <martin.leimer@zuehlke.com>
2020-07-25 22:12:56 +0800

    feat: add username and message input & send button

--- chat-web-app/src/features/Chat/Chat.js
+++ chat-web-app/src/features/Chat/Chat.js
@@ -2,11 +2,14 @@ import React, {useEffect, useState} from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import ChatMessageList from "./ChatMessageList/ChatMessageList";
 import {loadMessages} from "../../api/messageApi";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(() => ({
   container: {
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    flexDirection: 'column'
   }
 }));
 
@@ -14,6 +17,8 @@ function Chat() {
 
   const classes = useStyles();
   const [messages, setMessages] = useState([]);
+  const [userName, setUserName] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     loadMessages()
@@ -22,9 +27,18 @@ function Chat() {
       });
   }, [setMessages]);
 
+  const sendMessage = () => {
+    console.log(message);
+  }
+
   return (
     <div className={classes.container}>
       <ChatMessageList messages={messages}/>
+      <form noValidate autoComplete="off">
+        <TextField label="User name" value={userName} onChange={(event) => setUserName(event.target.value)}/>
+        <TextField label="Message" value={message} onChange={(event) => setMessage(event.target.value)}/>
+        <Button onClick={sendMessage} variant="contained" color="primary">Send</Button>
+      </form>
     </div>
   )
 }
