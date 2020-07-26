cf3ab2c9b8c123557968543373b5b5a74ab839d9
Martin Leimer <martin.leimer@zuehlke.com>
2020-07-26 11:15:59 +0800

    refactor: rename ChatAppApplication to ChatApplication

--- chat-server/src/main/java/com/mleimer/chat/ChatApplication.java
+++ chat-server/src/main/java/com/mleimer/chat/ChatApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatAppApplication {
+public class ChatApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatAppApplication.class, args);
+        SpringApplication.run(ChatApplication.class, args);
     }
 }
